3ec6a495ee1f6224fb807e67d1d486760ffda64a
mikespub <github@mikespub.net>
2025-01-15 15:44:59 +0100

    remove dummy main

--- class/usergui.php
+++ class/usergui.php
@@ -26,17 +26,5 @@ sys::import('modules.scheduler.class.userapi');
  */
 class UserGui extends UserGuiClass
 {
-    /**
-     * User main GUI function
-     * @param array<string, mixed> $args
-     * @return array<mixed>
-     */
-    public function main(array $args = [])
-    {
-        $args['description'] ??= 'Description of scheduler';
-
-        // Pass along the context for xarTpl::module() if needed
-        $args['context'] ??= $this->getContext();
-        return $args;
-    }
+    // ...
 }
